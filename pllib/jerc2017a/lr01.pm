f835033752b6e601195e2d1c6a224a280a226a80
Sophia Shapira <Sophia.Shapira@gmail.com>
2017-06-23 16:25:42 -0400

    Added the _safety_ and _bsc_ functions to layer 1.

--- pllib/jerc2017a/lr01.pm
+++ pllib/jerc2017a/lr01.pm
@@ -1,5 +1,36 @@
 package jerc2017a::lr01;
 use strict;
+use JSON::PP;
+
+
+sub bsc {
+  my $lc_ret;
+  my $lc_src;
+  my $lc_chr;
+  $lc_src = scalar reverse $_[0];
+  $lc_ret = "\'";
+  while ( $lc_src ne "" )
+  {
+    $lc_chr = chop($lc_src);
+    if ( $lc_chr eq "\'" ) { $lc_chr = "\'\"\'\"\'"; }
+    $lc_ret .= $lc_chr;
+  }
+  $lc_ret .= "\'";
+  return $lc_ret;
+}
+
+
+sub safety {
+  my $lc_ok;
+  my $lc_rsl;
+  $lc_ok = 0;
+  eval {
+    $lc_rsl = decode_json($_[0]);
+    $_[1] = $lc_rsl;
+    $lc_ok = 10;
+  };
+  return ( $lc_ok > 5 );
+}
 
 
 
